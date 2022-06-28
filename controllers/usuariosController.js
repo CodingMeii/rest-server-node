16e818d2f893ca805946a0d3d624999079b472cf
Andrea Gómez <91890800+CodingMeii@users.noreply.github.com>
2022-06-28 17:41:41 -0500

    Usuarios CRUD listo - 28 Jun 2020

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,32 +1,69 @@
 const {response, request} = require('express');
+const bcryptjs = require('bcryptjs');
+const Usuario = require('../models/usuario');
 
-const usuriosGet = (req = request, res = response) => {
-    const {q, nombre = 'No name', apikey} = req.query;
+const usuriosGet = async (req = request, res = response) => {
+    //const {q, nombre = 'No name', apikey} = req.query;
+    const {limite = 5, desde = 0} = req.query;
+
+    /*const usuarios = await Usuario.find({estado: true})
+        .skip(Number(desde))
+        .limit(Number(limite));
+
+    const total = await Usuario.countDocuments({estado: true});*/
+
+    //Ejecutar al mismo tiempo dos await
+    const [total, usuarios] = await Promise.all([
+        Usuario.countDocuments({estado: true}),
+        Usuario.find({estado: true})
+        .skip(Number(desde))
+        .limit(Number(limite))
+    ])
 
     res.json({
-        msg:'get API - Controlador',
-        q,
-        nombre,
-        apikey
+        total,
+        usuarios
     });
 }
 
-const usuariosPost = (req, res = response) => {
-    const {nombre, edad} = req.body;
+const usuariosPost = async (req, res = response) => {
+    const {nombre, correo, password, rol} = req.body;
+    const usuario = new Usuario({nombre, correo, password, rol});
+
+    //Verificar si el correo existe
+    /*const existeEmail = await Usuario.findOne({correo});
+    if(existeEmail){
+        return res.status(400).json({
+            msg: 'El correo ingresado ya esta registrado'
+        })
+    }*/
+
+    //Encriptar la contraseña
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync(password, salt);
+
+    //Guardar en DB
+    await usuario.save();
 
     res.json({
-        msg:'post API - Controlador',
-        nombre,
-        edad
+        usuario
     });
 }
 
-const usuariosPut = (req, res = response) => {
-    const id = req.params.id;
+const usuariosPut = async (req, res = response) => {
+    const {id} = req.params;
+    const {_id, password, google, correo, ...resto} = req.body;
+
+    //TODO validar contra la base de datos
+    if(password){
+        const salt = bcryptjs.genSaltSync();
+        resto.password = bcryptjs.hashSync(password, salt);
+    }
+
+    const usuario = await Usuario.findOneAndUpdate(id, resto);
 
     res.json({
-        msg:'put API - Controlador',
-        id
+        usuario
     });
 }
 
@@ -36,9 +73,16 @@ const usuariosPatch = (req, res = response) => {
     });
 }
 
-const usuariosDelete = (req, res = response) => {
+const usuariosDelete = async (req, res = response) => {
+    const {id} = req.params;
+
+    //fisicamente se borra
+    //const usuario = await Usuario.findByIdAndDelete(id);
+
+    const usuario = await Usuario.findByIdAndUpdate(id, {estado: false});
+
     res.json({
-        msg:'delete API - Controlador'
+        usuario
     });
 }
 
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,15 +1,35 @@
 const {Router} = require('express');
+const {check} = require('express-validator');
 const { usuriosGet, usuariosPost, usuariosPut, usuariosDelete, usuariosPatch } = require('../controllers/usuariosController');
+const { esRolValido, emailExiste, existeUsarioPorId } = require('../helpers/db-validators');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 router.get('/', usuriosGet);
 
-router.put('/:id', usuariosPut);
+router.put('/:id', [
+    check('id', 'No es un ID valido').isMongoId(),
+    check('id').custom(id => existeUsarioPorId(id)),
+    check('rol').custom(rol => esRolValido(rol)),
+    validarCampos
+], usuariosPut);
 
-router.post('/', usuariosPost);
+router.post('/', [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password debe tener mas de 6 letras').isLength({min: 6}),
+    //check('correo', 'El correo no es valido').isEmail(),
+    //check('rol', 'No es un rol valido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('correo').custom(email => emailExiste(email)),
+    check('rol').custom(rol => esRolValido(rol)),
+    validarCampos
+], usuariosPost);
 
-router.delete('/', usuariosDelete);
+router.delete('/:id', [
+    check('id', 'No es un ID valido').isMongoId(),
+    check('id').custom(id => existeUsarioPorId(id)),
+    validarCampos
+], usuariosDelete);
 
 router.patch('/', usuariosPatch);
 
